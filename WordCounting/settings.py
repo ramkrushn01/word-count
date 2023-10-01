d191c0a33c179f74e1364707332d78b182933630
ramkrushn <ramkrushnsalunkhe99@gmail.com>
2023-10-01 21:36:02 +0530

    make collect_static

--- WordCounting/settings.py
+++ WordCounting/settings.py
@@ -134,12 +134,17 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static_')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # add by ramkrushn
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
