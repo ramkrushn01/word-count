c1b58bb8fd11c55ad5f93ffd662e0ad4e73b2a21
ramkrushn <ramkrushnsalunkhe99@gmail.com>
2023-10-01 20:25:26 +0530

    add the about us page content and design, create deployment changes for rendor.com

--- WordCounting/settings.py
+++ WordCounting/settings.py
@@ -77,16 +77,22 @@ WSGI_APPLICATION = 'WordCounting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wordcount',
-        'USER': 'root',
-        'PASSWORD': 'ramkrushn@123,./',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'wordcount',
+    #     'USER': 'root',
+    #     'PASSWORD': 'ramkrushn@123,./',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 
 
 # Password validation
